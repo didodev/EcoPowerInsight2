e3839940d39d44f97d19c75ec7b4fe855b7086a0
didodev <104768426+didodev@users.noreply.github.com>
2024-06-07 20:07:19 +0100

    Add files via upload

---
+++ build/precache-manifest.488a1aa786f754549aafc06876096ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbadc07333ddd1776734ecb38b08ea0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214d7a9b3bd1891e6e71",
+    "url": "/static/css/main.bea44771.chunk.css"
+  },
+  {
+    "revision": "34b0f5cc551343af925b",
+    "url": "/static/js/2.e8b958e0.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "214d7a9b3bd1891e6e71",
+    "url": "/static/js/main.43eea2ce.chunk.js"
+  },
+  {
+    "revision": "d9255067eef9f997e00f",
+    "url": "/static/js/runtime~main.f8e697fc.js"
+  }
+]);
